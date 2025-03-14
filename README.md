9bd93cdd6048e2c52ead3514b24d4be6f1c649ca
duvallviera <Duval.vera002@mymdc.net>
2025-03-14 08:42:55 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Filter_Filters
+<<<<<<< HEAD
+
+# Face Detection using OpenCV and Streamlit
+=======
+
+# Face Detection using OpenCV and Streamlit
+>>>>>>> a2fdd737516741bfe25865812474812deef52751
---
+++ setup.sh
@@ -0,0 +1,21 @@
+<<<<<<< HEAD
+mkdir -p ~/.streamlit/
+echo "
+[server]\n
+headless = true\n
+enableCORS=false\n
+enableXsrfProtection=false\n
+port = $PORT\n
+\n
+" > ~/.streamlit/config.toml
+=======
+mkdir -p ~/.streamlit/
+echo "
+[server]\n
+headless = true\n
+enableCORS=false\n
+enableXsrfProtection=false\n
+port = $PORT\n
+\n
+" > ~/.streamlit/config.toml
+>>>>>>> a2fdd737516741bfe25865812474812deef52751
